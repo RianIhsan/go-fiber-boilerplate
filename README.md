a0dccbf34e12d8a5f1e8617f8143351b2bf5a999
RianIhsan <rianwhatsup@gmail.com>
2023-05-17 00:52:19 +0700

    Setup gofiber

---
+++ README.md
@@ -0,0 +1,23 @@
+# GOLANG SETUP BACKEND with FIBER Framework
+
+---
+
+## Stack :
+
+- Golang (Gofiber)
+
+---
+
+## Package :
+
+- Gofiber
+- Validator
+- Get Env
+- Database mysql
+- Database postgres
+- Database sqlite
+- ORM
+
+---
+
+Learning by doing
---
+++ boot/bootApp.go
@@ -0,0 +1,45 @@
+package boot
+
+import (
+	"log"
+	"os"
+
+	"github.com/RianIhsan/Go-Fiber-Boilerplate/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+)
+
+func BootApp() {
+	//LOAD ENV FILE
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error load .env file")
+	}
+
+	//CHECK DEFAULT PORT
+	if portEnv := os.Getenv("PORT"); portEnv != "" {
+		config.PORT = portEnv
+	}
+
+	config.BootDatabase()
+	config.ConnectDB()
+	config.GoMigration()
+
+	app := fiber.New()
+
+	//Cors config
+	app.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOrigins:     config.AllowOrigins,
+		AllowMethods:     config.AllowMethods,
+		AllowHeaders:     config.AllowHeaders,
+		AllowCredentials: config.AllowCredentials,
+		ExposeHeaders:    config.ExposeHeaders,
+		MaxAge:           config.MaxAge,
+	}))
+
+	//Route Init
+	routes.SetupRoute(app)
+
+	app.Listen(config.PORT)
+}
